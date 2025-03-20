147718a8d0663c5da2d54b0478ad0baffb61d216
bereket2sh <bereketshimels77@gmail.com>
2025-03-20 13:02:05 -0500

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,7 @@
- 
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/',  // If using custom domain
+  // OR
+  // base: '/repository-name/',  // If using github.io/repository-name
+}) 
