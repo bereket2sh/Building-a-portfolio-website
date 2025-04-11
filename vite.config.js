647d78c2a50c2586c2474bbe17ed10896d46037a
bereket2sh <bereketshimels77@gmail.com>
2025-04-10 20:10:31 -0500

    Fix remove all profiles and add image

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/Building-a-portfolio-website/',  // Must match your repository name exactly
+  base: '/Building-a-portfolio-website/',  // Must match exactly
   // OR
   // base: '/repository-name/',  // If using github.io/repository-name
 }) 
